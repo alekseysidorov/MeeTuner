48b4d36afcd90b26f03df39322d1532c69cf66c1
Aleksey Sidorov <gorthauer87@ya.ru>
2012-01-29 22:32:59 +0400

    Updates and new classes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <QtGui/QApplication>
 #include "qmlapplicationviewer.h"
+
 #include "analyzer/frequencyanalyzer.h"
+#include "analyzer/noteanalyzer.h"
+
 #include <QtDeclarative>
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
     QScopedPointer<QApplication> app(createApplication(argc, argv));
 
-    qmlRegisterType<FrequencyAnalyzer>("org.meetuner", 1, 0, "Analyzer");
+    qmlRegisterType<FrequencyAnalyzer>("org.meetuner", 1, 0, "FrequencyAnalyzer");
+    qmlRegisterType<NoteAnalyzer>("org.meetuner", 1, 0, "NoteAnalyzer");
 
     QmlApplicationViewer viewer;
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
